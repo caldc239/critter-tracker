9e2bdf99ebc1043676925a514a20fdb94613c188
caldc239 <55357449+caldc239@users.noreply.github.com>
2020-05-04 16:09:36 -0400

    Added comments Areas for lists Created JS file

---
+++ CTScript.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+/*File for JavaScript to support Critter Tracker
+Created by Cassaundra Caldwell and Rey Orozco
+Date Created: 4 May 2020
+Date Last Modified: 4 May 2020
+File name: CTScripts.js */
