7e6ebf56b8a79cffa1bb7dbe76c6cf6204e1a94a
caldc239 <55357449+caldc239@users.noreply.github.com>
2020-05-04 23:53:23 -0400

    Moved addToList and updateList functions out of addButton function Created updateList function to write user selection to list on page Put "completedList" in its own div element

--- CTScript.js
+++ CTScript.js
@@ -7,22 +7,34 @@ Date Created: 4 May 2020
 Date Last Modified: 4 May 2020
 File name: CTScripts.js */
 
-window.onload = function() {addButton()};
+window.onload = function() {
+  addButton()
+};
+
+/*array to hold IDs of critters obtained from name*/
+var userCritters = [];
 
 function addButton() {
   document.getElementById("addBtn").addEventListener("click", addToList);
+};
 
-  function addToList() {
-    /*clear text box*/
-    document.getElementById('critterSelection').value = '';
-    /*var list = document.getElementById('addedList');
-    var critter = document.getElementById('critterSelection').value;
-    var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(critter));
-    list.appendChild(entry);*/
-    var critter = document.getElementById('critterSelection').value;
-    var userCritters =[];
+function addToList() {
+  /*var list = document.getElementById('addedList');
+  var critter = document.getElementById('critterSelection').value;
+  var entry = document.createElement('li');
+  entry.appendChild(document.createTextNode(critter));
+  list.appendChild(entry);*/
+  var critter = document.getElementById('critterSelection').value;
+  userCritters.push(critters_lookup_by_name(critter));
+  updateList();
+  /*clear text box*/
+  document.getElementById('critterSelection').value = '';
+};
 
-
-  };
+function updateList() {
+  document.getElementById('addedList').innerHTML = "";
+  var i;
+  for (i = 0; i < userCritters.length; ++i) {
+    document.getElementById('addedList').innerHTML += "<li>" + bugs_data[userCritters[i] - 1]["name"] + "</li>";
+  }
 };
